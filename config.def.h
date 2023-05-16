7fe93f9db13bf51f0ca707772beba876127230f3
Pete Neisen <pete@neisen.xyz>
2023-05-16 09:15:13 -0600

    Patched in bottomstack

--- config.def.h
+++ config.def.h
@@ -60,6 +60,8 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
+  { "TTT",      bstack },
+  { "===",      bstackhoriz },
 	{ "[@]",      spiral },
 	{ "[\\]",     dwindle },
 	{ "|M|",      centeredmaster },
@@ -117,6 +119,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+  { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+  { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
